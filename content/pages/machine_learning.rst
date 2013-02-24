214bd40a7c040621339b1f219bd8671ed905e995
Olivier Grisel <olivier.grisel@ensta.org>
2013-02-24 23:12:47 +0100

    Initial ogrisel.com website structure using pelican

---
+++ content/pages/machine_learning.rst
@@ -0,0 +1,8 @@
+Machine Learning
+================
+
+:date: 2013-02-24 10:20
+:slug: machine-learning
+:author: Olivier Grisel
+
+TODO: paragraph on what is machine learning and why it's useful.
---
+++ pelicanconf.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'Olivier Grisel'
+SITENAME = u'Olivier Grisel'
+SITESUBTITLE = u"Freelance Machine Learning Expert"
+SITEURL = 'http://ogrisel.com'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+THEME = u'themes/ogrisel.com'
+
+# Blogroll
+LINKS =  ()
+
+# Social widget
+SOCIAL = (('@ogrisel on twitter', 'http://twitter.com/ogrisel'),
+          ('@ogrisel on github', 'http://github.com/ogrisel'),
+          ('LinkedIn profile', 'http://linkedin.com/olivier.grisel'),
+          )
+
+DEFAULT_PAGINATION = 10
+
+DISPLAY_PAGES_ON_MENU = ('machine_learning.rst',)
